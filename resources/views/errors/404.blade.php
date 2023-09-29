3e824fa2e853a870f680dfa7be315f7d8b3ac009
Dimas Mahendra Kusuma <dimasmahendrakusuma@gmail.com>
2023-09-29 16:47:44 +0700

    update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <title>404 | Shrikrisna </title>
     <link rel="stylesheet" href="/cms/css/cms.css?v={{ $version }}">
     <link rel="stylesheet" href="/cms/css/bootstrap.css">
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
 <head>
     <title>Login | Shrikrisna</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <link rel="shortcut icon" href="{{ $faviconcompany }}" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="/cms/vendors/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="/cms/vendors/toastify/toastify.css">
